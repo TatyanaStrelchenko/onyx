b667df2eb59a8c1bf226be6b0a18c830327ed55b
vladimir <strelchenkovladimir.s@gmail.com>
2022-06-30 14:10:15 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -147,47 +147,47 @@ function App() {
                 </p>
               </div>
               <div className="col">
-                <h4>Loan programs</h4>
-                {/* <ul>
-                  <li><Link to="/">Multifamily</Link>  </li>
-                  <li><Link to="/">New Construction</Link></li>
-                  <li><Link to="/">Fix and Flip</Link></li>
-                  <li><Link to="/">Rental</Link></li>
-                  <li><Link to="/">Fix to Rent</Link></li>
-                  <li><Link to="/">Commercial</Link></li>
-                  <li><Link to="/">Bridge</Link></li>
-                </ul> */}
-              </div>
-              <div className="col">
-                <h4>Property Types</h4>
-                {/* <ul>
-                  <li><Link to="/">Offices</Link></li>
-                  <li><Link to="/">Retail</Link></li>
-                  <li><Link to="/">Self-Storage</Link></li>
-                  <li><Link to="/">Hospitality</Link></li>
-                  <li><Link to="/">Inductrial</Link></li>
-                  <li><Link to="/">Multifamily</Link></li>
-                  <li><Link to="/">Manufacturated Housing</Link></li>
-                </ul> */}
-              </div>
-              <div className="col">
-                <h4>Services</h4>
-                {/* <ul>
-                  <li><Link to="/">Debt</Link></li>
-                  <li><Link to="/">Equity</Link></li>
-                </ul>  */}
-              </div>
-              <div className="col">
-                <h4>Quick Navigation</h4>
-                {/* <ul>
-                  <li><Link to="/">About Us</Link></li>
-                  <li><Link to="/">Contact Us</Link></li>
-                  <li><Link to="/">Blog</Link></li>
-                  <li><Link to="/">Partners</Link></li>
-                </ul> */}
-                <h4>Address</h4>
+                <h4>LOAN PROGRAMS</h4>
                 <ul>
-                  <li>2200 NW 2nd Ave, STE 211, Miami, FL 33127</li>
+                  <li>Multifamily</li>
+                  <li>New Construction</li>
+                  <li>Fix and Flip</li>
+                  <li>Rental</li>
+                  <li>Fix to Rent</li>
+                  <li>Commercial</li>
+                  <li>Bridge</li>
+                </ul> 
+              </div>
+              <div className="col">
+                <h4>PROPERTY TYPES</h4>
+                <ul>
+                  <li>Offices</li>
+                  <li>Retail</li>
+                  <li>Self-Storage</li>
+                  <li>Hospitality</li>
+                  <li>Inductrial</li>
+                  <li>Multifamily</li>
+                  <li>Manufacturated Housing</li>
+                </ul>
+              </div>
+              <div className="col">
+                <h4>SERVICES</h4>
+                <ul>
+                  <li>Debt</li>
+                  <li>Equity</li>
+                </ul> 
+              </div>
+              <div className="col">
+                <h4>QUCK NAVIGATION</h4>
+                <ul>
+                  <li>About Us</li>
+                  <li>Contact Us</li>
+                  <li>Blog</li>
+                  <li>Partners</li>
+                </ul>
+                <h4>ADDRESS</h4>
+                <ul>
+                  <li>2200 NW 2nd Ave, STE<br/> 211, Miami, FL 33127</li>
                   <li>support@onyxfunding.us</li>
                   <li>(415) 583-5515</li>
                 </ul>
@@ -195,12 +195,12 @@ function App() {
             </div>
             <div className="footer-bottom">
               <p>© Copyright - Onyx - 2022</p>
-              {/* <ol>
-                <li><Link to="/">facebook</Link></li>
-                <li><Link to="/">Instagram</Link></li>
-                <li><Link to="/">Behance</Link></li>
-                <li><Link to="/">Linkedin</Link></li>
-              </ol> */}
+               <ul className="social">
+                <li>facebook</li>
+                <li>Instagram</li>
+                <li>Behance</li>
+                <li>Linkedin</li>
+              </ul>
             </div>
           </div>
         </div>
