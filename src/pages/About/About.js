5d9827153a79ca7964d029592d4ee93a4d024bfa
vladimir <strelchenkovladimir.s@gmail.com>
2022-07-25 15:54:59 +0300

    style

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -83,6 +83,37 @@ const About = () => {
           </div>
         </div>
       </section>
+      <section className="promo-section">
+        <div className="container">
+          <div className="info-holder">
+            <div className="info-block">
+              <p>Experience</p>
+              <p className="info-text">
+                We have partnered with thousands of borrowers to finance investment properties
+                across the nation. We leverage our expertise and relationships to deliver you
+                outstanding results.
+              </p>
+            </div>
+            <div className="info-block">
+              <p>Competitive Rates</p>
+              <p className="info-text">
+                Our vast lender relationships, and everyday market experience enable us to customize
+                unparalleled financing solutions for any type of investment property.
+              </p>
+            </div>
+            <div className="info-block">
+              <p>Team Mindset</p>
+              <p className="info-text">
+                From underwriting to final payoff, Onyx Funding is by your side every step of the
+                way. We strive to provide an outstanding experience - Every time. No exceptions.
+              </p>
+            </div>
+          </div>
+          <button type="button" className="btn btn-default">
+            GET STARTED
+          </button>
+        </div>
+      </section>
       <Footer />
     </div>
   );
