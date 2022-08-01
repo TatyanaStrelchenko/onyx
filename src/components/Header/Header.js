db829c06d2b9b60bf6d85e7d3e342b3d52d60427
Tatyana Strelchenko <Tatyana.Strelchenko@dataart.com>
2022-08-01 12:56:43 +0300

    fix menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import './Header.scss';
 import Logo from '../../assets/image/logo.svg';
 
 const Header = () => {
+  const ref = useRef();
+
   const [isBurgerMenuOpen, setBurgerMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setBurgerMenuOpen(!isBurgerMenuOpen);
+  };
+
+  const closeMenu = () => {
+    console.log('test', isBurgerMenuOpen);
+    setBurgerMenuOpen(!isBurgerMenuOpen);
+  };
 
   return (
     <header className="header">
@@ -18,20 +29,19 @@ const Header = () => {
         <div className="header-info">
           <button
             type="button"
-            className={`burger ${isBurgerMenuOpen ? '' : 'open-menu'}`}
-            onClick={() => setBurgerMenuOpen(!isBurgerMenuOpen)}
+            className={`burger ${isBurgerMenuOpen ? 'open-menu' : ''}`}
+            onClick={toggleMenu}
           >
             <span className="burger-button" />
             <div className="spinner3 diagonal part-1"> </div>
             <div className="spinner3 horizontal"> </div>
             <div className="spinner3 diagonal part-2"> </div>
           </button>
-          <ul
-            className={`nav-menu ${isBurgerMenuOpen ? 'close' : 'open'}`}
-            isOpen={isBurgerMenuOpen}
-          >
+          <ul ref={ref} className={`nav-menu ${isBurgerMenuOpen ? 'open' : 'close'}`}>
             <li>
-              <Link to="/about">ABOUT US</Link>
+              <Link to="/about" onClick={() => closeMenu()}>
+                ABOUT US
+              </Link>
             </li>
             <li>
               <Link to="/loan-programs">LOAN PROGRAMS</Link>
