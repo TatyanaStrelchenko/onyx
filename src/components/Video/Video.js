6bc767f4e45272f489227e366d742bbc18c847d5
Bond252018 <nikoncort21@gmail.com>
2022-08-22 12:26:54 +0300

    fix video

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable react/jsx-no-undef */
 import Fade from 'react-reveal/Fade';
 import './Video.scss';
@@ -28,10 +29,16 @@ const Video = () => {
           src="https://drive.google.com/file/d/11VWy4XLoHlwkjzDT_lscEphlfOKwyMiO/preview"
           width="100%"
           height="100%"
-          border="0"
+          autoPlay="1"
+          mute="1"
           allow="autoplay"
+          showinfo="1"
         />
       </div>
+      {/* <video className="video-holder" width="100%" height="1000px" >
+<source src="https://drive.google.com/file/d/11VWy4XLoHlwkjzDT_lscEphlfOKwyMiO/preview?start=1" type="video/mp4"/>
+ 
+ </video> */}
     </div>
   );
 };
