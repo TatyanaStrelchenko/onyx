fe04c74db88001112997cf02d0aeee40737dfbb0
Bond252018 <nikoncort21@gmail.com>
2022-09-05 19:09:30 +0300

    fix new page and fix tabs services-page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { FeaturedLoanPrograms } from './pages/FeaturedLoanPrograms';
 import { Services } from './pages/Services';
 import { Equity } from './pages/Equity';
 import { Debt } from './pages/Debt';
+import { Investment } from './pages/Investment';
+import { Advisory } from './pages/Advisory';
 import { Partners } from './pages/Partners';
 import { Contact } from './pages/Contact';
 import './App.scss';
@@ -19,6 +21,8 @@ function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/services/equity" element={<Equity />} />
         <Route path="/services/debt" element={<Debt />} />
+        <Route path="/services/investment" element={<Investment />} />
+        <Route path="/services/advisory" element={<Advisory />} />
         <Route path="/partners" element={<Partners />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
