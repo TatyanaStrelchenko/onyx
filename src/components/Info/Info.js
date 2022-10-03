b593e9d5a283e7da6951471b73a5d5abe3c47f94
Tatyana Strelchenko <Tatyana.Strelchenko@dataart.com>
2022-10-03 21:01:50 +0300

    add widgets

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,180 +1,161 @@
 import './Info.scss';
 import Fade from 'react-reveal/Fade';
-import { ModalComponent } from '../Modal';
+import { useEffect, useRef } from 'react';
 
 const Info = () => {
+  // const widget = useRef();
+
+  const destroyFunc = useRef();
+  const effectCalled = useRef(false);
+  const renderAfterCalled = useRef(false);
+
+  if (effectCalled.current) {
+    renderAfterCalled.current = true;
+  }
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src =
+      'https://www.thefinancials.com/Widget.aspx?pid=ONYX&wid=0375808058&mode=js&width=0';
+    script.async = true;
+
+    const widgetOne = document.getElementById('widgetOne');
+    widgetOne.appendChild(script);
+
+    return () => {
+      widgetOne.removeChild(script);
+      if (!renderAfterCalled.current) {
+        return;
+      }
+      if (destroyFunc.current) {
+        destroyFunc.current();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const widget = document.createElement('script');
+    widget.type = 'text/javascript';
+    widget.src =
+      'https://www.thefinancials.com/Widget.aspx?pid=ONYX&wid=0375908060&mode=js&width=0';
+    widget.async = true;
+
+    const widgetTwo = document.getElementById('widgetTwo');
+    widgetTwo.appendChild(widget);
+
+    return () => {
+      widgetTwo.removeChild(widget);
+    };
+  }, []);
+
   return (
-    <>
+    <div className="info-page">
       <section className="info-section section">
         <div className="container">
           <div className="rates-holder">
             <Fade duration={5000}>
               <div className="rates-block">
+                <h2>PROUDLY CELEBRATING 30 YEARS</h2>
+                <h3>RATE INFORMATION IS AT LEAST 15 MINUTES DELAYED.</h3>
                 <p>
-                  Onyx Funding is a national, full-service commercial real estate firm with a focus
-                  on financing and loan origination. We are dedicated to delivering best-in-class
-                  solutions to investors, developers, and operators.
+                  Onyx Funding is changing the way real estate investors connect with capital
+                  sources. We work for you, the Borrower, and not for a Lender. This allows for
+                  complete transparency, synergy and alignment in our interests.
                 </p>
                 <p>
-                  As a relationship-based firm, we always prioritize our clients’ interests ahead of
-                  transactions. We believe in providing our clients with the highest standard of
-                  service, execution, integrity, and excellence.
+                  Since our inception we have helped hundreds of real estate investors achieve their
+                  goals by successfully negotiating and securing favorable debt and equity
+                  financing.
                 </p>
-                <ModalComponent customClass="btn-default" />
+                <button type="button" className="btn btn-default">
+                  Get Started
+                </button>
               </div>
             </Fade>
             <Fade duration={5000}>
               <div className="rates-block">
                 <div className="table-holder">
                   <h3>LIVE INTEREST RATES</h3>
-                  <div className="row">
-                    <div className="col">SOFR</div>
-                    <div className="col">2.28000%</div>
-                    <div className="col">2.28000%</div>
-                    <div className="col">2.28000%</div>
-                    <div className="col">0.05000%</div>
+                  <div id="widgetOne" />
+                  {/* <div className="row">
+                    <div className="col">1-MONTH LIBOR</div>
+                    <div className="col">2.862%</div>
+                    <div className="col">2.940%</div>
+                    <div className="col">2.655%</div>
                   </div>
                   <div className="row">
-                    <div className="col">30-Day Average SOFR</div>
-                    <div className="col">2.28536%</div>
-                    <div className="col">2.28536%</div>
-                    <div className="col">1.80760%</div>
-                    <div className="col">0.05000%</div>
+                    <div className="col">SOFR 30-DAY AVG</div>
+                    <div className="col">2.862%</div>
+                    <div className="col">2.940%</div>
+                    <div className="col">2.655%</div>
                   </div>
                   <div className="row">
-                    <div className="col">90-Day Average SOFR</div>
-                    <div className="col">1.82148%</div>
-                    <div className="col">1.80440%</div>
-                    <div className="col">1.30262%</div>
-                    <div className="col">0.04689%</div>
+                    <div className="col">SOFR 30-DAY TERM</div>
+                    <div className="col">2.862%</div>
+                    <div className="col">2.940%</div>
+                    <div className="col">2.655%</div>
                   </div>
                   <div className="row">
-                    <div className="col">1-month Term SOFR</div>
-                    <div className="col">2.74750%</div>
-                    <div className="col">2.68340%</div>
-                    <div className="col">2.30339%</div>
-                    <div className="col">0.05199%</div>
-                  </div>
-                  <div className="row">
-                    <div className="col">3-month Term SOFR</div>
-                    <div className="col">3.13380%</div>
-                    <div className="col">3.06459%</div>
-                    <div className="col">2.69280%</div>
-                    <div className="col">0.05236%</div>
-                  </div>
+                    <div className="col">PRIME RATE</div>
+                    <div className="col">2.862%</div>
+                    <div className="col">2.940%</div>
+                    <div className="col">2.655%</div>
+                  </div> */}
                 </div>
                 <div className="table-holder">
                   <h3>U.S. TREASURY LIBOR SWAP SOFR SWAP</h3>
-                  <div className="row">
-                    <div className="col">1 YEAR</div>
-                    <div className="col">3.703%</div>
-                    <div className="col">3.642%</div>
-                    <div className="col">3.327%</div>
-                    <div className="col">0.059%</div>
+                  <div id="widgetTwo" />
+                  {/* <div className="row">
+                    <div className="col">5-YEAR</div>
+                    <div className="col">2.862%</div>
+                    <div className="col">2.940%</div>
+                    <div className="col">2.655%</div>
                   </div>
                   <div className="row">
-                    <div className="col">2 YEAR</div>
-                    <div className="col">3.583%</div>
-                    <div className="col">3.531%</div>
-                    <div className="col">3.189%</div>
-                    <div className="col">0.175%</div>
+                    <div className="col">7-YEAR</div>
+                    <div className="col">2.862%</div>
+                    <div className="col">2.940%</div>
+                    <div className="col">2.655%</div>
                   </div>
                   <div className="row">
-                    <div className="col">3 YEAR</div>
-                    <div className="col">3.403%</div>
-                    <div className="col">3.363%</div>
-                    <div className="col">2.971%</div>
-                    <div className="col">0.347%</div>
+                    <div className="col">10-YEAR</div>
+                    <div className="col">2.862%</div>
+                    <div className="col">2.940%</div>
+                    <div className="col">2.655%</div>
                   </div>
                   <div className="row">
-                    <div className="col">5 YEAR</div>
-                    <div className="col">3.173%</div>
-                    <div className="col">3.150%</div>
-                    <div className="col">2.673%</div>
-                    <div className="col">0.655%</div>
-                  </div>
-                  <div className="row">
-                    <div className="col">7 YEAR</div>
-                    <div className="col">3.085%</div>
-                    <div className="col">3.071%</div>
-                    <div className="col">2.554%</div>
-                    <div className="col">0.864%</div>
-                  </div>
-                  <div className="row">
-                    <div className="col">10 YEAR</div>
-                    <div className="col">3.063%</div>
-                    <div className="col">3.058%</div>
-                    <div className="col">2.525%</div>
-                    <div className="col">1.067%</div>
-                  </div>
-                  <div className="row">
-                    <div className="col">15 YEAR</div>
-                    <div className="col">3.083%</div>
-                    <div className="col">3.086%</div>
-                    <div className="col">2.576%</div>
-                    <div className="col">1.248%</div>
-                  </div>
-                  <div className="row">
-                    <div className="col">30 YEAR</div>
-                    <div className="col">2.814%</div>
-                    <div className="col">2.822%</div>
-                    <div className="col">2.401%</div>
-                    <div className="col">1.359%</div>
-                  </div>
+                    <div className="col">20-YEAR</div>
+                    <div className="col">2.862%</div>
+                    <div className="col">2.940%</div>
+                    <div className="col">2.655%</div>
+                  </div> */}
                 </div>
               </div>
-              {/* START theFinancials.com Content – SOFR/Prime */}
-
-              {/* copyright theFinancials.com - All Rights Reserved */}
-
-              {/* Get Help by Calling 1.843.886.3635 */}
-
-              {/* specify the width of this Widget by changing '&width=0' at the end of the installation code. Use '&width=100%' to force the Widget to fill its parent container or leave it as 0 for default width */}
-
-              <script
-                type="text/javascript"
-                src="https://www.thefinancials.com/Widget.aspx?pid=ONYX&wid=0375808058&mode=js&width=0"
-              />
-
-              {/* END theFinancials.com Content */}
-
-              {/* START theFinancials.com Content – Treasuries/Swaps  */}
-
-              {/* copyright theFinancials.com - All Rights Reserved  */}
-
-              {/* Get Help by Calling 1.843.886.3635  */}
-
-              {/* specify the width of this Widget by changing '&width=0' at the end of the installation code. Use '&width=100%' to force the Widget to fill its parent container or leave it as 0 for default width  */}
-
-              <script
-                type="text/javascript"
-                src="https://www.thefinancials.com/Widget.aspx?pid=ONYX&wid=0375908060&mode=js&width=0"
-              />
-
-              {/* END theFinancials.com Content  */}
             </Fade>
           </div>
         </div>
       </section>
+
       <section className="number-section">
         <div className="container">
           <div className="number-holder">
             <div className="numbers">
-              <span className="number">$250M</span>
+              <span className="number">$20BN</span>
               <Fade duration={5000}>
-                <p className="text">Loans Funded</p>
+                <p className="text">Loans Closed</p>
               </Fade>
             </div>
             <div className="numbers">
-              <span className="number">216</span>
+              <span className="number">$8.5BN</span>
               <Fade duration={5000}>
-                <p className="text">Deals Closed</p>
+                <p className="text">Units Funded</p>
               </Fade>
             </div>
             <div className="numbers">
-              <span className="number">250+</span>
+              <span className="number">565</span>
               <Fade duration={5000}>
-                <p className="text">Lender Relationships</p>
+                <p className="text">States</p>
               </Fade>
             </div>
           </div>
@@ -188,7 +169,7 @@ const Info = () => {
               </p>
             </div>
             <div className="info-block">
-              <p>Competitive Terms</p>
+              <p>Competitive Rates</p>
               <p className="info-text">
                 Our vast lender relationships, and everyday market experience enable us to customize
                 unparalleled financing solutions for any type of investment property.
@@ -202,10 +183,12 @@ const Info = () => {
               </p>
             </div>
           </div>
-          <ModalComponent customClass="btn-default" />
+          <button type="button" className="btn btn-default">
+            Get Started
+          </button>
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
